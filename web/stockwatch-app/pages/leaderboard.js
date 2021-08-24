f5d7e0cec23bca557a8527d392b6bcd74707f129
Katie Langerman <langermank@gmail.com>
2021-08-24 11:35:05 -0700

    leaderboard changes

--- web/stockwatch-app/pages/leaderboard.js
+++ web/stockwatch-app/pages/leaderboard.js
@@ -1,6 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { useTable } from 'react-table';
-import { Button, Grid, Header, Input, Loader, Table } from 'semantic-ui-react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useTable, usePagination, useExpanded } from 'react-table';
+// import Button from '../components/Button.jsx';
+// import Input from '../components/Input.jsx';
+import { Button, Loader } from 'semantic-ui-react';
 import { fetchAllTimeLeaderboard } from '../copied/RealityStockWatchBackend';
 import _ from 'lodash';
 
@@ -42,6 +44,25 @@ const columns = [
         accessor: (row) => row.networth.toFixed(2),
     },
 ];
+
+//  const columns = React.useMemo(
+
+//    () => [
+//         {
+//             Header: 'Rank',
+//             accessor: 'rank',
+//         },
+//         {
+//             Header: 'Player',
+//             accessor: 'player',
+//         },
+//         {
+//             Header: 'Net Worth',
+//             accessor: (row) => row.networth.toFixed(2),
+//     ],
+//    []
+//  )
+
 function updatePagerParams(setPagerParams, setNextTokens, changes) {
     setPagerParams((oldParams) => {
         let newParams = _.assign(_.cloneDeep(oldParams), changes);
@@ -99,157 +120,232 @@ const Leaderboard = ({ initialData, initialToken, initialPage }) => {
         fetchPage(pagerParams, setData, nextTokens, setNextTokens);
     }, [pagerParams]);
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
-        columns,
-        data,
-    });
+    // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    //     columns,
+    //     data,
+    // });
+
+    const tableInstance = useTable(
+        {
+            columns,
+            data,
+            initialState: { pageIndex: 2 },
+        },
+        usePagination
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        page, // Instead of using 'rows', we'll use page,
+        // which has only the rows for the active page
+
+        // The rest of these things are super handy, too ;)
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
+    } = tableInstance;
+
     console.log('nextTokens', pagerParams, nextTokens);
     // When the filter changes, the filter can be updated before nextTokens
     // is updated.
-    let pages = [<Loader key="loader" active inline="centered" />];
-    if (pagerParams.filter in nextTokens) {
-        const keys = Object.keys(nextTokens[pagerParams.filter]);
-        pages = keys.map((p) => {
-            if (p == 0) return '';
-            return (
-                <Button
-                    primary={p == pagerParams.currentPage}
-                    key={p}
-                    content={p}
-                    onClick={() =>
-                        updatePagerParams(setPagerParams, setNextTokens, {
-                            currentPage: parseInt(p),
-                        })
-                    }
-                />
-            );
-        });
-        if (nextTokens[pagerParams.filter][keys.length - 1]) {
-            pages.push(<Button key="dotdotdot" disabled icon="ellipsis horizontal" />);
-        }
-    }
+    // let pages = [<Loader key="loader" active inline="centered" />];
+    // if (pagerParams.filter in nextTokens) {
+    //     const keys = Object.keys(nextTokens[pagerParams.filter]);
+    //     pages = keys.map((p) => {
+    //         if (p == 0) return '';
+    //         return (
+    //             <Button
+    //                 primary={p == pagerParams.currentPage}
+    //                 key={p}
+    //                 content={p}
+    //                 onClick={() =>
+    //                     updatePagerParams(setPagerParams, setNextTokens, {
+    //                         currentPage: parseInt(p),
+    //                     })
+    //                 }
+    //             />
+    //         );
+    //     });
+    //     if (nextTokens[pagerParams.filter][keys.length - 1]) {
+    //         pages.push(<Button key="dotdotdot" disabled icon="ellipsis horizontal" />);
+    //     }
+    // }
     return (
         <>
-            <Header as="h2">All-time Leaderboard</Header>
-            <Grid centered>
-                <Grid.Row>
-                    <Grid.Column width={4}>
-                        <Input
-                            action={{
-                                icon: 'search',
-                                disabled: pagerParams.filter === filterInput,
-                                onClick: () =>
-                                    updatePagerParams(setPagerParams, setNextTokens, {
-                                        filter: filterInput,
-                                    }),
-                            }}
-                            icon="users"
-                            iconPosition="left"
-                            placeholder="Search..."
-                            fluid
-                            value={filterInput}
-                            onChange={(e) => setFilterInput(e.target.value)}
-                            onKeyUp={(e) => {
-                                if (e.keyCode === 13) {
-                                    updatePagerParams(setPagerParams, setNextTokens, {
-                                        filter: filterInput,
-                                    });
-                                }
-                            }}
-                        />
-                    </Grid.Column>
-                    <Grid.Column width={2}>
-                        <Button.Group>
-                            <Button
-                                labelPosition="left"
-                                icon="left chevron"
-                                content="Back"
-                                disabled={pagerParams.currentPage == 1}
-                                onClick={() =>
-                                    updatePagerParams(setPagerParams, setNextTokens, {
-                                        currentPage: pagerParams.currentPage - 1,
-                                    })
-                                }
-                            />
-                            {pages}
-                            <Button
-                                labelPosition="right"
-                                icon="right chevron"
-                                content="Forward"
-                                disabled={
-                                    !(
-                                        nextTokens[pagerParams.filter] &&
-                                        nextTokens[pagerParams.filter][pagerParams.currentPage]
-                                    )
-                                }
-                                onClick={() =>
-                                    updatePagerParams(setPagerParams, setNextTokens, {
-                                        currentPage: pagerParams.currentPage + 1,
-                                    })
-                                }
-                            />
-                        </Button.Group>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+            <h2>All-time Leaderboard</h2>
+            <input
+                // action={{
+                //     icon: 'search',
+                //     disabled: pagerParams.filter === filterInput,
+                //     onClick: () =>
+                //         updatePagerParams(setPagerParams, setNextTokens, {
+                //             filter: filterInput,
+                //         }),
+                // }}
+                placeholder="Search..."
+                value={filterInput}
+                onChange={(e) => setFilterInput(e.target.value)}
+                // onKeyUp={(e) => {
+                //     if (e.keyCode === 13) {
+                //         updatePagerParams(setPagerParams, setNextTokens, {
+                //             filter: filterInput,
+                //         });
+                //     }
+                // }}
+            />
+            {/* <div>
+                <Button.Group>
+                    <Button
+                        labelPosition="left"
+                        icon="left chevron"
+                        content="Back"
+                        disabled={pagerParams.currentPage == 1}
+                        onClick={() =>
+                            updatePagerParams(setPagerParams, setNextTokens, {
+                                currentPage: pagerParams.currentPage - 1,
+                            })
+                        }
+                    />
+                    {pages}
+                    <Button
+                        labelPosition="right"
+                        icon="right chevron"
+                        content="Forward"
+                        disabled={
+                            !(
+                                nextTokens[pagerParams.filter] &&
+                                nextTokens[pagerParams.filter][pagerParams.currentPage]
+                            )
+                        }
+                        onClick={() =>
+                            updatePagerParams(setPagerParams, setNextTokens, {
+                                currentPage: pagerParams.currentPage + 1,
+                            })
+                        }
+                    />
+                </Button.Group>
+            </div> */}
+            <pre>
+                <code>
+                    {JSON.stringify(
+                        {
+                            pageIndex,
+                            pageSize,
+                            pageCount,
+                            canNextPage,
+                            canPreviousPage,
+                        },
+                        null,
+                        2
+                    )}
+                </code>
+            </pre>
 
-            <Table {...getTableProps()}>
-                <Table.Header>
+            <table {...getTableProps()}>
+                <thead>
                     {
                         // Loop over the header rows
                         headerGroups.map((headerGroup) => (
                             // Apply the header row props
-                            <Table.Row key="header" {...headerGroup.getHeaderGroupProps()}>
+                            <tr key="header" {...headerGroup.getHeaderGroupProps()}>
                                 {
                                     // Loop over the headers in each row
                                     headerGroup.headers.map((column) => (
                                         // Apply the header cell props
-                                        <Table.HeaderCell
-                                            key={column.id}
-                                            {...column.getHeaderProps()}>
+                                        <th key={column.id} {...column.getHeaderProps()}>
                                             {
                                                 // Render the header
                                                 column.render('Header')
                                             }
-                                        </Table.HeaderCell>
+                                        </th>
                                     ))
                                 }
-                            </Table.Row>
+                            </tr>
                         ))
                     }
-                </Table.Header>
+                </thead>
                 {/* Apply the table body props */}
-                <Table.Body {...getTableBodyProps()}>
+                <tbody {...getTableBodyProps()}>
                     {
                         // Loop over the table rows
-                        rows.map((row) => {
+                        page.map((row) => {
                             // Prepare the row for display
                             prepareRow(row);
                             return (
                                 // Apply the row props
-                                <Table.Row key={row.values['player']} {...row.getRowProps()}>
+                                <tr key={row.values['player']} {...row.getRowProps()}>
                                     {
                                         // Loop over the rows cells
                                         row.cells.map((cell) => {
                                             // Apply the cell props
                                             return (
-                                                <Table.Cell
-                                                    key={cell.column.id}
-                                                    {...cell.getCellProps()}>
+                                                <td key={cell.column.id} {...cell.getCellProps()}>
                                                     {
                                                         // Render the cell contents
                                                         cell.render('Cell')
                                                     }
-                                                </Table.Cell>
+                                                </td>
                                             );
                                         })
                                     }
-                                </Table.Row>
+                                </tr>
                             );
                         })
                     }
-                </Table.Body>
-            </Table>
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={(e) => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                            gotoPage(page);
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={(e) => {
+                        setPageSize(Number(e.target.value));
+                    }}>
+                    {[10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </>
     );
 };
