154947ed4a67d344f453d33ac25807b5c1a84ea9
Ami Ganguli <ami.ganguli@gmail.com>
2021-12-26 15:34:21 -0500

    Add timestamp to leaderboard

--- web/stockwatch-app/backend/config.js
+++ web/stockwatch-app/backend/config.js
@@ -1,2 +1,4 @@
 export const imageUrlPrefix = 'https://dsw9arc6h9tqj.cloudfront.net/';
 export const blankImage = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+export const leaderboardUrlPrefix =
+    'https://realitystockwatch-showassets113746-dev.s3.amazonaws.com/leaderboard/leaderboard-';
--- web/stockwatch-app/components/PlayerSelector.jsx
+++ web/stockwatch-app/components/PlayerSelector.jsx
@@ -3,28 +3,40 @@ import * as Popover from '@radix-ui/react-popover';
 import Input from 'components/Input';
 import { useLeaderboard } from 'backend/Leaderboard';
 
-const PlayerSelector = ({seasonID, onSelect}) => {
+const PlayerSelector = ({ seasonID, onSelect }) => {
     const { searchResults: players, searchTerm, setSearchTerm, setSeasonID } = useLeaderboard();
     useEffect(() => {
         setSeasonID(seasonID);
     }, [seasonID]);
 
-    const resultList = players?players.map((player) => <li key={player.playerID} onClick={()=>onSelect(player.playerID)}>
-        {player.displayName}
-    </li>):[];
-
-    return <>
-        <Popover.Root>
-            <Popover.Trigger >Add Player</Popover.Trigger>
-            <Popover.Anchor />
-            <Popover.Content>
-                <Popover.Close />
-                <Popover.Arrow />
-                <Input onChange={(e) => setSearchTerm(e.target.value)} placeholder="Find Player" />
-                <ul>{resultList}</ul>
-            </Popover.Content>
-        </Popover.Root>
-    </>;
+    const resultList = players ? (
+        <ul>
+            {players.map((player) => (
+                <li key={player.playerID} onClick={() => onSelect(player.playerID)}>
+                    {player.displayName}
+                </li>
+            ))}
+        </ul>
+    ) : (
+        <></>
+    );
+    return (
+        <>
+            <Popover.Root>
+                <Popover.Trigger>Add Player</Popover.Trigger>
+                <Popover.Anchor />
+                <Popover.Content>
+                    <Popover.Close />
+                    <Popover.Arrow />
+                    <Input
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        placeholder="Find Player"
+                    />
+                    {resultList}
+                </Popover.Content>
+            </Popover.Root>
+        </>
+    );
 };
 
 export default PlayerSelector;
